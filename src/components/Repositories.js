b340801a6113096938c08ad72a5695c03008a0be
Hannah Wolfe <github.erisds@gmail.com>
2021-11-12 16:33:26 +0000

    Cleaned up Repositories component
    - we don't need this fragment!

--- src/components/Repositories.js
+++ src/components/Repositories.js
@@ -5,31 +5,29 @@ import Octicon, {Repo, RepoForked, GitPullRequest, Tools, IssueOpened, Lock, Ver
 import './Repositories.css';
 
 export const Repositories = ({repositories = []}) => (
-    <>
-        <ul className="card-list">
-            {repositories.map(
-                ({name, id, descriptionHTML, url, hasIssuesEnabled, pullRequests, renovateRequests, issues, isFork, isPrivate, isMono}) => {
-                    return (
-                        <li key={id} className="card">
-                            <h4 className="title">
-                                <Octicon icon={isFork ? RepoForked : (isPrivate ? Lock : Repo)} />
-                                <a href={url}>{name}</a>
-                                {isMono ? <Octicon icon={Versions} size="medium" verticalAlign="middle" className="indicator" /> : null}
-                            </h4>
-                            <div className="desc" dangerouslySetInnerHTML={{__html: descriptionHTML}} />
-                            <div className="stats-box left">
-                                {hasIssuesEnabled ? <span className="stat"><Octicon icon={IssueOpened} />{issues.totalCount}</span> : null}
-                            </div>
-                            <div className="stats-box right">
-                                <span className="stat"><Octicon icon={GitPullRequest} />{pullRequests.totalCount}</span>
-                                <span className="stat"><Octicon icon={Tools} />{renovateRequests.totalCount}</span>
-                            </div>
-                        </li>
-                    );
-                }
-            )}
-        </ul>
-    </>
+    <ul className="card-list">
+        {repositories.map(
+            ({name, id, descriptionHTML, url, hasIssuesEnabled, pullRequests, renovateRequests, issues, isFork, isPrivate, isMono}) => {
+                return (
+                    <li key={id} className="card">
+                        <h4 className="title">
+                            <Octicon icon={isFork ? RepoForked : (isPrivate ? Lock : Repo)} />
+                            <a href={url}>{name}</a>
+                            {isMono ? <Octicon icon={Versions} size="medium" verticalAlign="middle" className="indicator" /> : null}
+                        </h4>
+                        <div className="desc" dangerouslySetInnerHTML={{__html: descriptionHTML}} />
+                        <div className="stats-box left">
+                            {hasIssuesEnabled ? <span className="stat"><Octicon icon={IssueOpened} />{issues.totalCount}</span> : null}
+                        </div>
+                        <div className="stats-box right">
+                            <span className="stat"><Octicon icon={GitPullRequest} />{pullRequests.totalCount}</span>
+                            <span className="stat"><Octicon icon={Tools} />{renovateRequests.totalCount}</span>
+                        </div>
+                    </li>
+                );
+            }
+        )}
+    </ul>
 );
 
 export const RepositoriesPlaceholder = () => (
